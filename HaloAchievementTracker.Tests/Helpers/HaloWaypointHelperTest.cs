c4644881d943fd3f853decc0324f386e39bbba56
Daniel Nilsson <daniel.nilsson94@outlook.com>
2020-07-16 10:43:46 +0200

    Added Halo 3 test data and improved test logic

--- HaloAchievementTracker.Tests/Helpers/HaloWaypointHelperTest.cs
+++ HaloAchievementTracker.Tests/Helpers/HaloWaypointHelperTest.cs
@@ -26,22 +26,33 @@ namespace HaloAchievementTracker.Tests.Helpers
         [Test]
         public void GetAchievements()
         {
+            var gameData = new List<(string Name, int AmountTotalAchievements, int AmountUnlockedAchievements)>
+            {
+                ("CrossGame", 80, 19),
+                ("HaloReach", 100, 29),
+                ("HaloCombatEvolved", 95, 31),
+                ("Halo2", 157, 34),
+                ("Halo3", 89, 26),
+                ("Halo3Odst", 98, 0),
+                ("Halo4", 81, 0),
+            };
+
+            int compoundAmountTotalAchievements = gameData.Sum(g => g.AmountTotalAchievements);
+            int compoundAmountUnlockedAchievements = gameData.Sum(g => g.AmountUnlockedAchievements);
+
             var achievements = helper.GetAchievements();
-
-            Assert.AreEqual(80, achievements.Where(a => a.GameId.Equals("CrossGame")).ToList().Count);
-            Assert.AreEqual(100, achievements.Where(a => a.GameId.Equals("HaloReach")).ToList().Count);
-            Assert.AreEqual(95, achievements.Where(a => a.GameId.Equals("HaloCombatEvolved")).ToList().Count);
-            Assert.AreEqual(157, achievements.Where(a => a.GameId.Equals("Halo2")).ToList().Count);
-            Assert.AreEqual(89, achievements.Where(a => a.GameId.Equals("Halo3")).ToList().Count);
-            Assert.AreEqual(98, achievements.Where(a => a.GameId.Equals("Halo3Odst")).ToList().Count);
-            Assert.AreEqual(81, achievements.Where(a => a.GameId.Equals("Halo4")).ToList().Count);
-
             var locked = achievements.Where(a => !a.IsUnlocked).ToList();
             var unlocked = achievements.Except(locked).ToList();
 
-            Assert.AreEqual(700, locked.Count() + unlocked.Count());
-            Assert.AreEqual(588, locked.Count());
-            Assert.AreEqual(112, unlocked.Count());
+            foreach (var (Name, AmountTotalAchievements, AmountUnlockedAchievements) in gameData)
+            {
+                Assert.AreEqual(AmountTotalAchievements, achievements.Where(a => a.GameId.Equals(Name)).ToList().Count);
+                Assert.AreEqual(AmountUnlockedAchievements, achievements.Where(a => a.GameId.Equals(Name) && a.IsUnlocked).ToList().Count);
+            }
+
+            Assert.AreEqual(compoundAmountTotalAchievements, locked.Count() + unlocked.Count());
+            Assert.AreEqual(compoundAmountTotalAchievements - compoundAmountUnlockedAchievements, locked.Count());
+            Assert.AreEqual(compoundAmountUnlockedAchievements, unlocked.Count());
         }
 
     }
