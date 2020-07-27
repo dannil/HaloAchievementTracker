9b508a3aaaa222146a4ed492eb60f6b982af7bf1
Daniel Nilsson <daniel.nilsson94@outlook.com>
2020-07-27 23:51:54 +0200

    Fixed lint errors (again)
    Also fixed up Travis lifecycle stages

--- HaloAchievementTracker.WebApp/ClientApp/src/app/misaligned-achievements/misaligned-achievements.component.spec.ts
+++ HaloAchievementTracker.WebApp/ClientApp/src/app/misaligned-achievements/misaligned-achievements.component.spec.ts
@@ -19,18 +19,18 @@ describe('MisalignedAchievementsComponent', () => {
     fixture.detectChanges();
   });
 
-  //it('should display a title', async(() => {
-  //  const titleText = fixture.nativeElement.querySelector('h1').textContent;
-  //  expect(titleText).toEqual('Counter');
-  //}));
+  // it('should display a title', async(() => {
+  //   const titleText = fixture.nativeElement.querySelector('h1').textContent;
+  //   expect(titleText).toEqual('Counter');
+  // }));
 
-  //it('should start with count 0, then increments by 1 when clicked', async(() => {
-  //  const countElement = fixture.nativeElement.querySelector('strong');
-  //  expect(countElement.textContent).toEqual('0');
+  // it('should start with count 0, then increments by 1 when clicked', async(() => {
+  //   const countElement = fixture.nativeElement.querySelector('strong');
+  //   expect(countElement.textContent).toEqual('0');
 
-  //  const incrementButton = fixture.nativeElement.querySelector('button');
-  //  incrementButton.click();
-  //  fixture.detectChanges();
-  //  expect(countElement.textContent).toEqual('1');
-  //}));
+  //   const incrementButton = fixture.nativeElement.querySelector('button');
+  //   incrementButton.click();
+  //   fixture.detectChanges();
+  //   expect(countElement.textContent).toEqual('1');
+  // }));
 });
