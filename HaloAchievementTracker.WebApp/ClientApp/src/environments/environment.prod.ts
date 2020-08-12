3080f1b425c7e0d910b4eb05bfe90b82242e63d9
Daniel Nilsson <daniel.nilsson94@outlook.com>
2020-08-12 14:56:37 +0200

    Lint

--- HaloAchievementTracker.WebApp/ClientApp/src/environments/environment.prod.ts
+++ HaloAchievementTracker.WebApp/ClientApp/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-import { IEnvironment } from "@environments/ienvironment";
+import { IEnvironment } from '@environments/ienvironment';
 
 export const environment: IEnvironment = {
   production: true,
