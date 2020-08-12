19440d320221d598e04c0346ac84e3da00512d20
Daniel Nilsson <daniel.nilsson94@outlook.com>
2020-08-12 15:02:18 +0200

    Lint

--- HaloAchievementTracker.WebApp/ClientApp/src/environments/environment.ts
+++ HaloAchievementTracker.WebApp/ClientApp/src/environments/environment.ts
@@ -2,7 +2,7 @@
 // `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-import { IEnvironment } from "@environments/ienvironment";
+import { IEnvironment } from '@environments/ienvironment';
 
 export const environment: IEnvironment = {
   production: false,
