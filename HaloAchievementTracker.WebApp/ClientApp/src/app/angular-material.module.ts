0b239722a8513ea7936fe7cfaa214647cfaffd0f
Daniel Nilsson <daniel.nilsson94@outlook.com>
2020-08-12 13:32:22 +0200

    Upgraded to Angular 10

--- HaloAchievementTracker.WebApp/ClientApp/src/app/angular-material.module.ts
+++ HaloAchievementTracker.WebApp/ClientApp/src/app/angular-material.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import {
-  MatButtonModule,
-  MatBadgeModule,
-  MatInputModule,
-  MatProgressSpinnerModule,
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   imports: [
