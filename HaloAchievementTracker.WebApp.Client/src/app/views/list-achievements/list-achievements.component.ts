bd2c8a91884cfc3b996fb86bf8e335c9fdafb7f1
Daniel Nilsson <daniel.nilsson94@outlook.com>
2020-12-23 20:02:43 +0100

    Added basic client side filtering

--- HaloAchievementTracker.WebApp.Client/src/app/views/list-achievements/list-achievements.component.ts
+++ HaloAchievementTracker.WebApp.Client/src/app/views/list-achievements/list-achievements.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { Observable, of } from 'rxjs';
+import { combineLatest, Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 import { AchievementsService } from '@app/services/achievements.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -13,10 +14,36 @@ import { AngularMaterialModule } from '@app/angular-material.module';
 })
 export class ListAchievementsComponent {
 
+  filter$: Observable<string>;
+  filterForm: FormGroup;
+
   achievements$: Observable<IAchievement[]>;
+  achievementsFiltered$: Observable<IAchievement[]>;
 
   constructor(private achievementsService: AchievementsService) {
+    this.filterForm = new FormGroup({
+      filter: new FormControl()
+    });
+
     this.achievements$ = this.achievementsService.getList();
+
+    this.filter$ = this.filterForm.get('filter').valueChanges.pipe(
+      startWith('')
+    );
+
+    this.achievementsFiltered$ = combineLatest([this.achievements$, this.filter$]).pipe(
+      map(([achievements, filter]) => {
+        return this.filter(achievements, filter);
+      })
+    )
+  }
+
+  filter(achievements: IAchievement[], filter: string): IAchievement[] {
+    return achievements.filter(achievement => {
+      return achievement.name.toLowerCase().includes(filter) ||
+        achievement.game.name.toLowerCase().includes(filter) ||
+        achievement.description.toLowerCase().includes(filter);
+    })
   }
 
 }
@@ -25,6 +52,8 @@ export class ListAchievementsComponent {
   imports: [
     CommonModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
     AngularMaterialModule
   ],
   declarations: [
