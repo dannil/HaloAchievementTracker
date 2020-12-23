59fe7693d9f1db9c061823e16ab56568fa68f2fe
Daniel Nilsson <daniel.nilsson94@outlook.com>
2020-12-23 20:16:08 +0100

    Changed code according to VS recommendations

--- HaloAchievementTracker.WebApp.API/Startup.cs
+++ HaloAchievementTracker.WebApp.API/Startup.cs
@@ -63,7 +63,7 @@ namespace HaloAchievementTracker.WebApp
             }
             else
             {
-                throw new ArgumentException();
+                throw new ArgumentException($"Xbox Live implementation {actualImplementation} is not a valid configuration.");
             }
         }
 
@@ -99,7 +99,7 @@ namespace HaloAchievementTracker.WebApp
             lifetime.ApplicationStarted.Register(OnApplicationStartedAsync(xboxLiveApiAdapter).Wait);
         }
 
-        private async Task<Action> OnApplicationStartedAsync(IXboxLiveApiAdapter xboxLiveApiAdapter)
+        private static async Task<Action> OnApplicationStartedAsync(IXboxLiveApiAdapter xboxLiveApiAdapter)
         {
             await xboxLiveApiAdapter.Warmup();
             return null;
